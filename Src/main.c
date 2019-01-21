bad68e9c0d055d257d3bc2fedecb5013c856e457
Adam Czarnas <aczarnas@inovatica.com>
2019-01-21 08:54:17 +0100

    cleanup in main file

--- Src/main.c
+++ Src/main.c
@@ -1,58 +1,5 @@
-/**
- ******************************************************************************
- * @file    LTDC/LTDC_Display_1Layer/Src/main.c
- * @author  MCD Application Team
- * @version V1.0.0
- * @date    25-June-2015
- * @brief   This example provides a description of how to configure LTDC peripheral
- *          to display BMP image on LCD using only one layer.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; COPYRIGHT(c) 2015 STMicroelectronics</center></h2>
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************
- */
-
-/* Includes ------------------------------------------------------------------*/
 #include <main.h>
 
-/** @addtogroup STM32F7xx_HAL_Examples
- * @{
- */
-
-/** @addtogroup LTDC_Display_1Layer
- * @{
- */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-
-/* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void CPU_CACHE_Enable(void);
 
@@ -114,56 +61,31 @@ void drawMandelbrotAlternative(const uint8_t scale, const uint16_t iterations,
 int main(void) {
 	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
-	/* Enable the CPU Cache */
 	CPU_CACHE_Enable();
 
-	/* STM32F7xx HAL library initialization:
-	 - Configure the Flash ART accelerator on ITCM interface
-	 - Systick timer is configured by default as source of time base, but user
-	 can eventually implement his proper time base source (a general purpose
-	 timer for example or other time source), keeping in mind that Time base
-	 duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
-	 handled in milliseconds basis.
-	 - Set NVIC Group Priority to 4
-	 - Low Level Initialization
-	 */
 	HAL_Init();
 
-	/* Configure the system clock to 216 MHz */
 	SystemClock_Config();
 
-	/* LCD clock configuration */
-	/* PLLSAI_VCO Input = HSE_VALUE/PLL_M = 1 Mhz */
-	/* PLLSAI_VCO Output = PLLSAI_VCO Input * PLLSAIN = 192 Mhz */
-	/* PLLLCDCLK = PLLSAI_VCO Output/PLLSAIR = 192/5 = 38.4 Mhz */
-	/* LTDC clock frequency = PLLLCDCLK / LTDC_PLLSAI_DIVR_4 = 38.4/4 = 9.6Mhz */
 	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
 	PeriphClkInitStruct.PLLSAI.PLLSAIN = 192;
 	PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
 	PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_4;
 	HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
 
-	/* Configure LCD : Only one layer is used */
 	BSP_LCD_Init();
+	BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
+	BSP_LCD_DisplayOn();
+	BSP_LCD_SelectLayer(0);
 
 	BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize());
 	BSP_TS_ITConfig();
 
-	/* LCD Initialization */
-	BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
-
-	/* Enable the LCD */
-	BSP_LCD_DisplayOn();
-
-	/* Select the LCD Background Layer  */
-	BSP_LCD_SelectLayer(0);
-
 	TS_StateTypeDef ts;
 	uint8_t globalScale = 1;
+	uint16_t iterations = 1;
 	uint16_t centerX = 240;
 	uint16_t centerY = 136;
-
-	uint16_t iterations = 1;
 
 	while (1) {
 		if (flag) {
@@ -180,32 +102,11 @@ int main(void) {
 	}
 }
 
-/**
- * @brief  System Clock Configuration
- *         The system Clock is configured as follow :
- *            System Clock source            = PLL (HSE)
- *            SYSCLK(Hz)                     = 216000000
- *            HCLK(Hz)                       = 216000000
- *            AHB Prescaler                  = 1
- *            APB1 Prescaler                 = 4
- *            APB2 Prescaler                 = 2
- *            HSE Frequency(Hz)              = 25000000
- *            PLL_M                          = 25
- *            PLL_N                          = 432
- *            PLL_P                          = 2
- *            PLL_Q                          = 9
- *            VDD(V)                         = 3.3
- *            Main regulator output voltage  = Scale1 mode
- *            Flash Latency(WS)              = 7
- * @param  None
- * @retval None
- */
 void SystemClock_Config(void) {
 	RCC_ClkInitTypeDef RCC_ClkInitStruct;
 	RCC_OscInitTypeDef RCC_OscInitStruct;
 	HAL_StatusTypeDef ret = HAL_OK;
 
-	/* Enable HSE Oscillator and activate PLL with HSE as source */
 	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
 	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
 	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -222,7 +123,6 @@ void SystemClock_Config(void) {
 		}
 	}
 
-	/* Activate the OverDrive to reach the 216 MHz Frequency */
 	ret = HAL_PWREx_EnableOverDrive();
 	if (ret != HAL_OK) {
 		while (1) {
@@ -230,7 +130,6 @@ void SystemClock_Config(void) {
 		}
 	}
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
 	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
 			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -246,28 +145,13 @@ void SystemClock_Config(void) {
 	}
 }
 
-/**
- * @brief  CPU L1-Cache enable.
- * @param  None
- * @retval None
- */
 static void CPU_CACHE_Enable(void) {
-	/* Enable I-Cache */
 	SCB_EnableICache();
-
-	/* Enable D-Cache */
 	SCB_EnableDCache();
 }
 
 #ifdef  USE_FULL_ASSERT
 
-/**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
 void assert_failed(uint8_t* file, uint32_t line)
 {
 	/* User can add his own implementation to report the file name and line number,
@@ -279,13 +163,3 @@ void assert_failed(uint8_t* file, uint32_t line)
 	}
 }
 #endif
-
-/**
- * @}
- */
-
-/**
- * @}
- */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
